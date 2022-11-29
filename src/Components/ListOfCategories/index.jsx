c346474e702f43d43bea4858088bdd13f74838b5
armando salamanca ayon <asalamanca.ayon@gmail.com>
2022-11-29 13:00:46 -0600

    add fake data to render the header app

--- src/Components/ListOfCategories/index.jsx
+++ src/Components/ListOfCategories/index.jsx
@@ -1,13 +1,16 @@
 import React from "react";
 import { Category } from "../Category";
 import { List, Item } from "./styles";
+import data from "../../../api/db.json";
 
 export const ListOfCategories = () => {
+    //TODO remove this when connect to graphql
+    const { categories } = data;
     return (
         <List>
-            {[1, 2, 3, 4].map((category) => (
-                <Item key={category}>
-                    <Category />
+            {categories.map(({ id, cover, name, emoji, path }) => (
+                <Item key={id}>
+                    <Category cover={cover} emoji={emoji} path={path} />
                 </Item>
             ))}
         </List>
