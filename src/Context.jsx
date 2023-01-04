3252ce7fff0f78756b12cdfb6eae2863fe7cc38a
Armando Salamanca Ayon <asalamanca.ayon@gmail.com>
2023-01-04 11:28:25 -0600

    React apollo (#4)
    * install and configure graphql and apollo to use the querys
    
    * add get single photo info graphql query
    
    * add toogle likes mutations
    
    * add mutation unlikeAnonymousPhoto
    
    * register new mutation unlikeAnonymousPhotos
    
    * add unlikeAnonymousPhoto and mutation in api folder
    
    * rever changes into schema js file
    
    * add mutation unlikeAnonymousPhoto
    
    * add functionality to unlike anonympos photo
    
    * add react route to navigate in many paths
    
    * add deatil page to navigation
    
    * add protected routes with react router dom v6
    
    * fix for redeclaration Browser router dependency
    
    * protected routes
    
    * add react context, userForm, new hook to hanlde the form inputs

--- src/Context.jsx
+++ src/Context.jsx
@@ -2,9 +2,12 @@ import React, { useState, createContext } from "react";
 const AppContext = createContext(null);
 const AppProvider = ({ children }) => {
     const [isAuth, setIsAuth] = useState(false);
+    const [isRegister, setRegister] = useState(true);
     const activeAuth = () => setIsAuth(true);
     return (
-        <AppContext.Provider value={{ isAuth, activeAuth }}>
+        <AppContext.Provider
+            value={{ isAuth, activeAuth, isRegister, setRegister }}
+        >
             {children}
         </AppContext.Provider>
     );
